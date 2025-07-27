457295bf832e45fa09191c5d3ed06031c3aeb813
Christian <lvchristian20@gmail.com>
2025-07-27 11:19:39 -0500

    fix: merge branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AddNews from "./pages/AddNews.tsx";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/content/new" element={<AddNews/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <Router>
+                <Routes>
+                    <Route path="/" element={<LoginForm />} />
+
+                    <Route
+                        path="/usuarios"
+                        element={
+                            <PrivateRoute allowedRoles={['admin']}>
+                                <h2>Vista protegida: Usuarios</h2>
+                            </PrivateRoute>
+                        }
+                    />
+
+                    <Route
+                        path="/editor"
+                        element={
+                            <PrivateRoute allowedRoles={['admin', 'editor']}>
+                                <h2>Vista protegida: Editor</h2>
+                            </PrivateRoute>
+                        }
+                    />
+
+                    <Route
+                        path="/perfil"
+                        element={
+                            <PrivateRoute>
+                                <h2>Vista protegida: Perfil de cualquier usuario</h2>
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+        </Router>
+);
 }
 
-export default App
+export default App;
